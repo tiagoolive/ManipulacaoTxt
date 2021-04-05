73854f1f67ce9d435d46f507f9412890ae5b7a62
tiago camargo de oliveira <tiago.olive.cam@hotmail.com>
2021-04-05 14:05:59 -0300

    Adicionar arquivos de projeto.

---
+++ ManipulaçãoTxt/Pessoas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManipulaçãoTxt
+{
+    public class Pessoas
+    {
+        public string Nome { get; set; }
+    }
+}
---
+++ ManipulaçãoTxt/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ManipulaçãoTxt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string caminho = "C:../../../../ManipulaçãoTxt/Prova Estagio DEV - Anexo 1.txt";
+            List<Pessoas> lista = new List<Pessoas>();
+            if (File.Exists(caminho))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(caminho))
+                    {
+                        string linha;
+                        while ((linha = reader.ReadLine()) != null){
+                            Pessoas pessoa = new Pessoas();
+                            if (linha.Contains("1"))
+                            {
+                                pessoa.Nome = linha;
+                                lista.Add(pessoa);
+                            }
+                            Console.WriteLine(linha);
+                        }
+                    }
+                foreach(Pessoas i in lista)
+                    {
+                        Console.WriteLine(i.Nome);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
+            else
+            {
+                Console.WriteLine($" O arquivo {caminho} não foi localizado");
+            }
+            
+        }
+
+    }
+}
